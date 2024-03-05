4278986afccf6df5640940f03c661344c60664fa
Selim Ellieh <selim.cod4r@gmail.com>
2024-03-05 19:27:02 +0200

    Authentication

---
+++ authentication/views.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import UserSerializer
+
+class RegisterUserView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({"user": serializer.data}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
