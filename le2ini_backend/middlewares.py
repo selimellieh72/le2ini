ae7e8bf5ed5eb5cbd21727b883642e378fc1c795
Selim Ellieh <selimplayselim@gmail.com>
2025-04-18 20:14:10 +0300

    fixed logresponsebodymiddleware

--- le2ini_backend/middlewares.py
+++ le2ini_backend/middlewares.py
@@ -1,17 +1,21 @@
 import logging
+from django.http import FileResponse
 
 class LogResponseBodyMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-        # Initialize your logging here
         self.logger = logging.getLogger(__name__)
 
     def __call__(self, request):
-        # Get the response from the view
         response = self.get_response(request)
-        
-        # Log the response content, be cautious with binary response
-      
-        self.logger.info(f"Response Body: {response.content.decode('utf-8')}")
+
+        if hasattr(response, 'content') and not isinstance(response, FileResponse):
+            try:
+                body = response.content.decode('utf-8')
+                self.logger.info(f"Response Body: {body}")
+            except UnicodeDecodeError:
+                self.logger.warning("Response content could not be decoded as UTF-8.")
+        else:
+            self.logger.debug(f"Skipped logging response body for path: {request.path} (streamed or binary content)")
 
         return response
