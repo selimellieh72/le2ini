d86550201e9dd3b47e4c6dd29b07c387db791781
Selim Ellieh <selimplayselim@gmail.com>
2025-04-18 20:10:53 +0300

    fixes

--- le2ini_backend/settings.py
+++ le2ini_backend/settings.py
@@ -65,6 +65,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -73,7 +74,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'le2ini_backend.middlewares.LogResponseBodyMiddleware',
-     "whitenoise.middleware.WhiteNoiseMiddleware"
+     
 ]
 
 LOGGING = {
